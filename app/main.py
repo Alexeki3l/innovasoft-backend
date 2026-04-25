2d3432c7336f159174dc7ce56420a2c686c713d0
Alexei Villafruela <104939017+Alexeki3l@users.noreply.github.com>
2026-04-25 14:58:28 -0400

    Agregar middleware CORS para permitir solicitudes desde el frontend

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.modules.auth.auth_router import router as auth_router
 from app.modules.client.client_router import router as client_router
 from app.modules.interest.interest_router import router as interest_router
@@ -11,6 +12,18 @@ app = FastAPI(
     title="Innovasoft API",
     description="API de gestión de clientes",
     version="1.0.0"
+)
+
+origins = [
+    "https://warm-begonia-4bbab5.netlify.app"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(auth_router, prefix="/api")
